d9068efa8eb9607c0b111b8908465b2e894f21d0
sarafanshul <anshulsaraf3@gmail.com>
2021-04-16 16:17:14 +0530

    added Item-On-Click-Listner for Recycler View

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This app use Recycler View and Card View at Core for card management
 
 - Date - 16-04-21
-	- start from base , colors and cards are working fine
 	[![prototype 1](/app_images/preview_v_1.png "prototype 1")](/app_images/preview_v_1.png "prototype 1")
+	- start from base , colors and cards are working fine
+	- added onclick listenr for recycler view
--- app/src/main/java/com/example/cardveiwapp/MainActivity.kt
+++ app/src/main/java/com/example/cardveiwapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.cardveiwapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,7 +12,6 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
 
 		// Recycler View
 		var _card_data = mutableListOf(
@@ -25,5 +27,16 @@ class MainActivity : AppCompatActivity() {
 		main_rv_main.adapter = _adapter
 		main_rv_main.layoutManager = LinearLayoutManager( this )
 
+		// onClick Listenr for Recycler View
+		main_rv_main.addOnItemTouchListener( RecyclerItemClickListenr( this , main_rv_main , object : RecyclerItemClickListenr.OnItemClickListener {
+			override fun onItemClick(view: View, position: Int) {
+				Log.d( "Short Click" , "Clicked" ) // test
+				Toast.makeText(this@MainActivity , "Short Click" , Toast.LENGTH_LONG).show()
+			}
+			override fun onItemLongClick(view: View?, position: Int) {
+				Log.d( "Long Click" , "Clicked" )
+				Toast.makeText(this@MainActivity , "Long Click" , Toast.LENGTH_LONG).show()
+			}
+		} ) )
 	}
 }
